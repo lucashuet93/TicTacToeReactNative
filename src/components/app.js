9d234927dafd2133113b6cfe6b8bbe5f679b667d
Lucas Huet-Hudson <lucas.huet.hudson@gmail.com>
2017-04-28 14:14:41 -0700

    style container

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import CheckerBoard from './CheckerBoard';
 class App extends React.Component {
     render() {
         return (
-            <CheckerBoard />
+            <View style={styles.container}>
+                <CheckerBoard />
+            </View>
         )
     }
 }
 export default App;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingLeft: 30,
+        paddingRight: 30,
+        paddingBottom: 30
+    }
+})
